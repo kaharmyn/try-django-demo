767e20d222ba42a2d20a2b11323874e72ca7a553
kaharmyn <akhayev.a@protonmail.com>
2023-09-12 14:35:15 +0600

    inital commit

--- articles/views.py
+++ articles/views.py
@@ -1,27 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from .forms import ArticleForm
 from .models import Article
 # Create your views here.
 
 def article_search_view(request):
-    query_dict = request.GET # this is a dictionary
-    #query = query_dict.get("q") #<input type="text" name="q"/>
-    # print(request.GET)
-    try:
-        query = int(query_dict.get("q"))
-    except:
-        query = None
-    article_obj = None
-    if query is not None:
-        article_obj = Article.objects.get(id=query)
+    query = request.GET.get('q')
+    qs = Article.objects.search(query=query)
     context = {
-        "object": article_obj,
+        "object_list": qs
     }
-    return render(request, 'articles/search.html',
-                  context=context)
-
+    return render(request, "articles/search.html", context=context)
 
 @login_required
 def article_create_view(request):
@@ -29,22 +20,44 @@ def article_create_view(request):
     context = {
         "form": form
     }
-
     if form.is_valid():
         article_object = form.save()
         context['form'] = ArticleForm()
+        # return redirect("article-detail", slug=article_object.slug)
+        return redirect(article_object.get_absolute_url())
+        # context['object'] = article_object
+        # context['created'] = True
+    return render(request, "articles/create.html", context=context)
 
-    return render(request, "articles/create.html",
-                  context=context)
+# def article_create_view(request):
+#     # print(request.POST)
+#     form = ArticleForm()
+#     context = {
+#         "form": form
+#     }
+#     if request.method == "POST":
+#         form = ArticleForm(request.POST)
+#         context['form'] = form
+#         if form.is_valid():
+#             title = form.cleaned_data.get("title")
+#             content = form.cleaned_data.get("content")
+#             article_object = Article.objects.create(title=title, content=content)
+#             context['object'] = article_object
+#             context['created'] = True
+#     return render(request, "articles/create.html", context=context)
 
-
-def article_detail_view(request, id=None):
+def article_detail_view(request, slug=None):
     article_obj = None
-    if id is not None:
-        article_obj = Article.objects.get(id=id)
+    if slug is not None:
+        try:
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
     context = {
         "object": article_obj,
-
     }
-    return render(request, "articles/detail.html",
-                  context=context)
+    return render(request, "articles/detail.html", context=context)
