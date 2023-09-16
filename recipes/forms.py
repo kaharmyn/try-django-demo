8c3cf5c432c452e99fad310e310ba46154704443
kaharmyn <akhayev.a@protonmail.com>
2023-09-16 14:53:11 +0600

    69 - Auto Save with HTMX & Django

--- recipes/forms.py
+++ recipes/forms.py
@@ -24,6 +24,10 @@ class RecipeForm(forms.ModelForm):
             new_data = {
                 "placeholder": f'Recipe {str(field)}',
                 "class": 'form-control',
+                "hx-post": ".",
+                "hx-trigger": "keyup changed delay:500ms",
+                "hx-terget": "#recipe-container",
+                "hx-swap": "outerHTML"
             }
             self.fields[str(field)].widget.attrs.update(
                 new_data
