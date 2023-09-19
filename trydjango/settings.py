9245a49549cf5ec40aa4ac1dc59f1b13ff6c9c09
kaharmyn <akhayev.a@protonmail.com>
2023-09-19 12:39:27 +0600

    76 - Upload Images in Django Models

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -166,3 +166,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / "staticfiles-cdn" / "uploads"
